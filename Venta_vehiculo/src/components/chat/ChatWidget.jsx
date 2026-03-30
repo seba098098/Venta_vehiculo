c94fe1b9a97814a0670525a7166b07d62824b05f
Sebastian Alvarez <seba098098@gmail.com>
2026-03-29 21:35:24 -0500

    fixes :finish

--- Venta_vehiculo/src/components/chat/ChatWidget.jsx
+++ Venta_vehiculo/src/components/chat/ChatWidget.jsx
@@ -275,8 +275,9 @@ const ChatWidget = () => {
             animate={{ opacity: 1, scale: 1, y: 0 }}
             exit={{ opacity: 0, scale: 0.95, y: 20 }}
             transition={{ duration: 0.25 }}
-            className="fixed inset-0 md:inset-auto md:bottom-20 md:right-4 md:w-[380px] md:h-[580px] 
-              bg-secondary md:rounded-2xl shadow-2xl flex flex-col overflow-hidden z-[9999] border-t md:border-t-0 border-white/10"
+            className="fixed bottom-24 left-4 right-4 h-[70vh] rounded-2xl 
+            md:inset-auto md:bottom-28 md:right-4 md:left-auto md:w-[380px] md:h-[580px] 
+            bg-secondary shadow-2xl flex flex-col overflow-hidden z-[9999] border border-white/10"
           >
             {/* HEADER */}
             <div className="flex items-center justify-between p-3 md:p-4 bg-surface-light border-b border-white/10 flex-shrink-0">
@@ -339,6 +340,20 @@ const ChatWidget = () => {
       >
         <MessageCircle className="w-5 h-5 md:w-6 md:h-6 text-white" />
       </motion.button>
+      <motion.div
+        initial={{ opacity: 0, x: 20 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 20 }}
+        transition={{ delay: 1 }}
+        className="fixed bottom-16 right-20 md:bottom-16 md:right-24 z-[9999]"
+      >
+        <div className="bg-white/20 backdrop-blur-md text-white px-4 py-2 rounded-xl shadow-lg text-sm whitespace-nowrap relative border border-white/30">
+          ¿Tienes alguna pregunta?
+
+          {/* flechita */}
+          <div className="absolute right-[-6px] top-1/2 -translate-y-1/2 w-3 h-3 bg-white rotate-45" />
+        </div>
+      </motion.div>
 
     </>
   );
